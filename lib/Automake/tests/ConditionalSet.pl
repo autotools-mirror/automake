149abece6e6ed39a6a625922cedd0bd343ff53cc
Alexandre Duret-Lutz <adl@gnu.org>
2002-11-19 20:02:40 +0000

    * lib/Automake/ConditionalSet.pm (_simplify, simplify): New methods. (true): Cache return value, so _simplify can use it. * lib/Automake/tests/ConditionalSet.pl (test_simplify): New function. * automake.in (variable_not_always_defined_in_cond): Return a simplified ConditionalSet. (macro_define, require_variables): Adjust. * tests/Makefile.am (TEST): Add library3.test. * tests/library3.test: New file. * tests/pluseq9.test: Adjust. Thanks to Raja R Harinath.

--- lib/Automake/tests/ConditionalSet.pl
+++ lib/Automake/tests/ConditionalSet.pl
@@ -92,7 +92,7 @@ sub test_permutations ()
       my $per = $set->permutations;
       if ($per != $res)
 	{
-	  print $per->string . ' != ' . $res->string . "\n";
+	  print " (P) " . $per->string . ' != ' . $res->string . "\n";
 	  return 1;
 	}
     }
@@ -137,14 +137,189 @@ sub test_invert ()
       my $inv = $set->invert;
       if ($inv != $res)
 	{
-	  print $inv->string . ' != ' . $res->string . "\n";
+	  print " (I) " . $inv->string . ' != ' . $res->string . "\n";
 	  return 1;
 	}
     }
   return 0;
 }
 
-exit (test_basics || test_permutations || test_invert);
+sub test_simplify ()
+{
+  my @tests = ([[["FOO_TRUE", "BAR_FALSE", "BAZ_FALSE"],
+		 ["FOO_TRUE", "BAR_FALSE", "BAZ_TRUE"]],
+		[["FOO_TRUE", "BAR_FALSE"]]],
+
+	       [[["FOO_TRUE", "BAR_FALSE", "BAZ_FALSE"],
+		 ["FOO_TRUE", "BAR_FALSE", "BAZ_TRUE"],
+		 ["FOO_TRUE", "BAR_TRUE"]],
+		[["FOO_TRUE"]]],
+
+	       [[["FOO_TRUE", "BAR_FALSE", "BAZ_FALSE"],
+		 ["FOO_TRUE", "BAR_FALSE", "BAZ_TRUE"],
+		 ["FOO_TRUE", "BAR_TRUE"],
+		 ["FOO_FALSE"]],
+		[["TRUE"]]],
+
+	       [[["FOO_TRUE", "BAR_FALSE", "BAZ_FALSE"],
+		 ["FOO_TRUE", "BAR_FALSE", "BAZ_TRUE"],
+		             ["BAR_TRUE",  "BAZ_TRUE"],
+		             ["BAR_FALSE", "BAZ_TRUE"]],
+		[["BAZ_TRUE"], ["FOO_TRUE", "BAR_FALSE"]]],
+
+	       [[["FOO_TRUE", "BAR_FALSE", "BAZ_FALSE"],
+		 ["FOO_TRUE", "BAR_FALSE", "BAZ_TRUE"],
+		             ["BAR_TRUE",  "BAZ_TRUE"],
+		             ["BAR_FALSE", "BAZ_TRUE"],
+		 ["FOO_FALSE"]],
+		# Note that this could be further simplified to
+		# [["FOO_FALSE"], ["BAZ_TRUE"], ["BAR_FALSE"]]
+		# but simplify isn't able to detect this.
+		[["FOO_FALSE"], ["BAZ_TRUE"], ["BAR_FALSE", "FOO_TRUE"]]],
+
+	       [[["B_TRUE"],
+		 ["A_FALSE", "B_TRUE"]],
+		[["B_TRUE"]]],
+
+	       [[["B_TRUE"],
+		 ["A_FALSE", "B_FALSE", "C_TRUE"],
+		 ["A_FALSE", "B_FALSE", "C_FALSE"]],
+		# Note that this could be further simplified to
+		# [["A_FALSE"], ["B_TRUE"]]
+		# but simplify isn't able to detect this.
+		[["A_FALSE", "B_FALSE"], ["B_TRUE"]]],
+
+	       [[["B_TRUE"],
+		 ["A_FALSE", "B_FALSE", "C_TRUE"],
+		 ["A_FALSE", "B_FALSE", "C_FALSE"],
+		 ["A_TRUE", "B_FALSE"]],
+		[["TRUE"]]],
+
+	       [[["A_TRUE", "B_TRUE"],
+		 ["A_TRUE", "B_FALSE"],
+		 ["A_TRUE", "C_FALSE", "D_FALSE"]],
+		[["A_TRUE"]]],
+
+	       [[["A_FALSE", "B_FALSE", "C_FALSE", "D_TRUE",  "E_FALSE"],
+		 ["A_FALSE", "B_FALSE", "C_TRUE",  "D_TRUE",  "E_TRUE"],
+		 ["A_FALSE", "B_TRUE",  "C_TRUE",  "D_FALSE", "E_TRUE"],
+		 ["A_FALSE", "B_TRUE",  "C_FALSE", "D_FALSE", "E_FALSE"],
+		 ["A_TRUE",  "B_TRUE",  "C_FALSE", "D_FALSE", "E_FALSE"],
+		 ["A_TRUE",  "B_TRUE",  "C_TRUE",  "D_FALSE", "E_TRUE"],
+		 ["A_TRUE",  "B_FALSE", "C_TRUE",  "D_TRUE",  "E_TRUE"],
+		 ["A_TRUE",  "B_FALSE", "C_FALSE", "D_TRUE",  "E_FALSE"]],
+		[           ["B_FALSE", "C_FALSE", "D_TRUE",  "E_FALSE"],
+		            ["B_FALSE", "C_TRUE",  "D_TRUE",  "E_TRUE"],
+		            ["B_TRUE",  "C_TRUE",  "D_FALSE", "E_TRUE"],
+		            ["B_TRUE",  "C_FALSE", "D_FALSE", "E_FALSE"]]],
+
+	       [[["A_FALSE", "B_FALSE", "C_FALSE", "D_TRUE",  "E_FALSE"],
+		 ["A_FALSE", "B_FALSE", "C_TRUE",  "D_TRUE",  "E_TRUE"],
+		 ["A_FALSE", "B_TRUE",  "C_TRUE",  "D_FALSE", "E_TRUE"],
+		 ["A_FALSE", "B_TRUE",  "C_FALSE", "D_FALSE", "E_FALSE"],
+		 ["A_TRUE",  "B_TRUE",  "C_FALSE", "D_FALSE", "E_FALSE"],
+		 ["A_TRUE",  "B_TRUE",  "C_TRUE",  "D_FALSE", "E_TRUE"],
+		 ["A_TRUE",  "B_FALSE", "C_TRUE",  "D_TRUE",  "E_TRUE"],
+		 ["A_TRUE",  "B_FALSE", "C_FALSE", "D_TRUE",  "E_FALSE"],
+		 ["A_FALSE", "B_FALSE", "C_FALSE", "D_FALSE", "E_FALSE"],
+		 ["A_FALSE", "B_FALSE", "C_TRUE",  "D_FALSE", "E_TRUE"],
+		 ["A_FALSE", "B_TRUE",  "C_TRUE",  "D_TRUE",  "E_TRUE"],
+		 ["A_FALSE", "B_TRUE",  "C_FALSE", "D_TRUE",  "E_FALSE"],
+		 ["A_TRUE",  "B_TRUE",  "C_FALSE", "D_TRUE",  "E_FALSE"],
+		 ["A_TRUE",  "B_TRUE",  "C_TRUE",  "D_TRUE",  "E_TRUE"],
+		 ["A_TRUE",  "B_FALSE", "C_TRUE",  "D_FALSE", "E_TRUE"],
+		 ["A_TRUE",  "B_FALSE", "C_FALSE", "D_FALSE", "E_FALSE"]],
+		[["C_FALSE", "E_FALSE"],
+		 ["C_TRUE", "E_TRUE"]]],
+
+	       [[["A_FALSE"],
+		 ["A_TRUE", "B_FALSE"],
+		 ["A_TRUE", "B_TRUE", "C_FALSE"],
+		 ["A_TRUE", "B_TRUE", "C_TRUE", "D_FALSE"],
+		 ["A_TRUE", "B_TRUE", "C_TRUE", "D_TRUE", "E_FALSE"],
+		 ["A_TRUE", "B_TRUE", "C_TRUE", "D_TRUE", "E_TRUE", "F_FALSE"],
+		 ["A_TRUE", "B_TRUE", "C_TRUE", "D_TRUE", "E_TRUE"]],
+		[["TRUE"]]],
+
+	       # Simplify should work with up to 31 variables.
+	       [[["V01_TRUE", "V02_TRUE", "V03_TRUE", "V04_TRUE", "V05_TRUE",
+		  "V06_TRUE", "V07_TRUE", "V08_TRUE", "V09_TRUE", "V10_TRUE",
+		  "V11_TRUE", "V12_TRUE", "V13_TRUE", "V14_TRUE", "V15_TRUE",
+		  "V16_TRUE", "V17_TRUE", "V18_TRUE", "V19_TRUE", "V20_TRUE",
+		  "V21_TRUE", "V22_TRUE", "V23_TRUE", "V24_TRUE", "V25_TRUE",
+		  "V26_TRUE", "V27_TRUE", "V28_TRUE", "V29_TRUE", "V30_TRUE",
+		  "V31_TRUE"],
+		 ["V01_TRUE", "V02_TRUE", "V03_TRUE", "V04_TRUE", "V05_TRUE",
+		  "V06_TRUE", "V07_TRUE", "V08_TRUE", "V09_TRUE", "V10_TRUE",
+		  "V11_TRUE", "V12_TRUE", "V13_TRUE", "V14_TRUE", "V15_TRUE",
+		  "V16_TRUE", "V17_TRUE", "V18_TRUE", "V19_TRUE", "V20_TRUE",
+		  "V21_TRUE", "V22_TRUE", "V23_TRUE", "V24_TRUE", "V25_TRUE",
+		  "V26_TRUE", "V27_TRUE", "V28_TRUE", "V29_TRUE", "V30_TRUE",
+		  "V31_FALSE"],
+		 ["V01_FALSE","V02_TRUE", "V03_TRUE", "V04_TRUE", "V05_TRUE",
+		  "V06_TRUE", "V07_TRUE", "V08_TRUE", "V09_TRUE", "V10_TRUE",
+		  "V11_TRUE", "V12_TRUE", "V13_TRUE", "V14_TRUE", "V15_TRUE",
+		  "V16_TRUE", "V17_TRUE", "V18_TRUE", "V19_TRUE", "V20_TRUE",
+		  "V21_TRUE", "V22_TRUE", "V23_TRUE", "V24_TRUE", "V25_TRUE",
+		  "V26_TRUE", "V27_TRUE", "V28_TRUE", "V29_TRUE", "V30_TRUE",
+		  "V31_TRUE"],
+		 ["V01_FALSE","V02_TRUE", "V03_TRUE", "V04_TRUE", "V05_TRUE",
+		  "V06_TRUE", "V07_TRUE", "V08_TRUE", "V09_TRUE", "V10_TRUE",
+		  "V11_TRUE", "V12_TRUE", "V13_TRUE", "V14_TRUE", "V15_TRUE",
+		  "V16_TRUE", "V17_TRUE", "V18_TRUE", "V19_TRUE", "V20_TRUE",
+		  "V21_TRUE", "V22_TRUE", "V23_TRUE", "V24_TRUE", "V25_TRUE",
+		  "V26_TRUE", "V27_TRUE", "V28_TRUE", "V29_TRUE", "V30_TRUE",
+		  "V31_FALSE"]],
+		[[            "V02_TRUE", "V03_TRUE", "V04_TRUE", "V05_TRUE",
+		  "V06_TRUE", "V07_TRUE", "V08_TRUE", "V09_TRUE", "V10_TRUE",
+		  "V11_TRUE", "V12_TRUE", "V13_TRUE", "V14_TRUE", "V15_TRUE",
+		  "V16_TRUE", "V17_TRUE", "V18_TRUE", "V19_TRUE", "V20_TRUE",
+		  "V21_TRUE", "V22_TRUE", "V23_TRUE", "V24_TRUE", "V25_TRUE",
+		  "V26_TRUE", "V27_TRUE", "V28_TRUE", "V29_TRUE", "V30_TRUE"
+		  ]]]);
+
+  for my $t (@tests)
+    {
+      my $set = build_set @{$t->[0]};
+      my $res = build_set @{$t->[1]};
+
+      # Make sure simplify() yields the expected result.
+      my $sim = $set->simplify;
+      if ($sim != $res)
+	{
+	  print " (S1) " . $set->string . "\n\t"
+	    . $sim->string . ' != ' . $res->string . "\n";
+	  return 1;
+	}
+
+      # Make sure simplify() is idempotent.
+      my $sim2 = $sim->simplify;
+      if ($sim2 != $sim)
+	{
+	  print " (S2) " . $sim->string . "\n\t"
+	    . $sim2->string . ' != ' . $sim->string . "\n";
+	  return 1;
+	}
+
+      # Also exercize invert() while we are at it.
+
+      # FIXME: Don't run invert() with too much conditionals, this is too slow.
+      next if $#{$t->[0][0]} > 8;
+
+      my $inv1 = $set->invert->simplify;
+      my $inv2 = $sim->invert->simplify;
+      if ($inv1 != $inv2)
+	{
+	  print " (S3) " . $set->string . ", " . $sim->string . "\n\t"
+	    . $inv1->string . ' != ' . $inv2->string . "\n";
+	  return 1;
+	}
+    }
+
+  return 0;
+}
+
+exit (test_basics || test_permutations || test_invert || test_simplify);
 
 ### Setup "GNU" style for perl-mode and cperl-mode.
 ## Local Variables:
