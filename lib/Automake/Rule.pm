f83d84f889456cdce7e9741d5e87d8f305b631d1
Ralf Wildenhues <Ralf.Wildenhues@gmx.de>
2006-08-28 16:04:24 +0000

    * doc/automake.texi, lib/Automake/Rule.pm: Fix some typos. * NEWS: Likewise.  Clarify that only one LIBOBJDIR is supported.

--- lib/Automake/Rule.pm
+++ lib/Automake/Rule.pm
@@ -1,4 +1,4 @@
-# Copyright (C) 2003, 2004  Free Software Foundation, Inc.
+# Copyright (C) 2003, 2004, 2006  Free Software Foundation, Inc.
 
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
@@ -631,7 +631,7 @@ sub define ($$$$$)
 	      if ($target !~ /^[^%]*%[^%]*$/)
 		{
 		  ## FIXME: Presently we can't diagnose duplicate user rules
-		  ## because we doesn't distinguish rules with commands
+		  ## because we don't distinguish rules with commands
 		  ## from rules that only add dependencies.  E.g.,
 		  ##   .PHONY: foo
 		  ##   .PHONY: bar
