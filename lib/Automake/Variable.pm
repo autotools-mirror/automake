53ac1719ca6b8ecaf1b64176ec72905432a7d8f4
Ralf Wildenhues <Ralf.Wildenhues@gmx.de>
2010-04-20 07:43:59 +0200

    Fix -Werror handling for presence of configure.in and configure.ac.
    * lib/Automake/Variable.pm (Automake::Variable): Do not initialize
    $configure_ac at the global level, before command-line arguments
    have been parsed.
    (require_variables): Initialize it here.
    * tests/configure.test: New test.
    * tests/Makefile.am: Update.
    Report by Stefano Lattarini.

--- lib/Automake/Variable.pm
+++ lib/Automake/Variable.pm
@@ -193,7 +193,7 @@ my %_ac_macro_for_var =
    );
 
 # The name of the configure.ac file.
-my $configure_ac = find_configure_ac;
+my $configure_ac;
 
 # Variables that can be overridden without complaint from -Woverride
 my %_silent_variable_override =
@@ -1131,6 +1131,9 @@ sub require_variables ($$$@)
   my ($where, $reason, $cond, @vars) = @_;
   my $res = 0;
   $reason .= ' but ' unless $reason eq '';
+
+  $configure_ac = find_configure_ac
+    unless defined $configure_ac;
 
  VARIABLE:
   foreach my $var (@vars)
