c349db06721d376cb70be78450e78d30a476910d
Stefano Lattarini <stefano.lattarini@gmail.com>
2011-07-17 13:38:42 +0200

    testsuite: scaffolding to allow use of TAP in our own tests
    Now that Automake has initial support for the TAP test protocol,
    we can start "eating our own dog food" and rewrite some tests to
    use TAP; this should allow for better granularity, especially in
    the control of skips and expected failures.  With this change, we
    set up the initial scaffolding required by the planned TAP tests,
    and convert few older tests to use TAP, mostly in order to verify
    that there are no obvious errors.  The tests in our testsuite that
    use the TAP protocol will be marked by the new `.tap' extension.
    
    * tests/plain-functions.sh: New file containing definition of
    helper shell functions used by the "protocol-less" tests of the
    Automake testsuite.
    * tests/tap-functions.sh:  New file containing definition of
    helper shell functions used by the TAP-producing tests of the
    Automake testsuite.
    * tests/defs: Remove definitions of some functions that are
    now defined in `plain-functions.sh'.  Instead, source one of
    `plain-functions.sh' or `tap-functions.sh', depending on whether
    `$use_tap' is set to "no" or "yes".
    (exit trap): Call `late_plan_' if `$use_tap' is true.  Also,
    unset shell traces before issuing the latest commands, to avoid
    confusing the tap driver with spurious output.
    * tests/defs-static.in ($use_tap): New variable, by default set
    to "yes" if the calling test script has a `.tap' suffix, and to
    "no" otherwise.  The individual scripts can override it though.
    In code sanity-checking the environment, verify that `$use_tap'
    is not exported.
    * tests/self-check-env-sanitize.test: Update, and small related
    reformatting.
    * tests/self-check-tap.test: New very minimal self test.
    * tests/acoutbs.test, tests/acoutbs2.test, tests/acoutnoq.test,
    tests/acoutpt.test, tests/acoutpt2.test, tests/acoutqnl.test:
    Removed, merged into ...
    * tests/ac-output-old.tap: ... this new test, that uses TAP.
    * tests/Makefile.am (TAP_LOG_DRIVER): Define to invoke our own
    `tap-driver' script.
    (TAP_LOG_DRIVER_FLAGS): Define to `--merge', so that the stdout
    and stderr of the test scripts remains synced.
    (TEST_EXTENSIONS): Define, to add the `.tap' suffix; also list
    the `.test' suffix explicitly.
    (EXTRA_DIST): Distribute the new files `plain-functions.sh' and
    `tap-functions.sh'.
    ($(TEST_LOGS)): Depends on them.
    (AM_TESTS_ENVIRONMENT): Ensure that `use_tap' is not exported in
    the environment of the tests.
    (TESTS): Update.

---
+++ tests/tap-functions.sh
@@ -0,0 +1,239 @@
+# -*- shell-script -*-
+#
+# Copyright (C) 2011 Free Software Foundation, Inc.
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2, or (at your option)
+# any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+# Helper functions used by TAP-producing tests of the Automake testsuite.
+
+#
+# IMPORTANT: All the functions defined in this file can *not* be used
+#            from within a subshell, unless explicitly noted otherwise.
+#
+
+# The count of the TAP test results seen so far.
+tap_count_=0
+
+# The first "test -n" tries to avoid extra forks when possible.
+if test -n "${ZSH_VERSION}${BASH_VERSION}" \
+     || (eval 'test $((1 + 1)) = 2') >/dev/null 2>&1
+then
+  # Use of 'eval' needed to protect dumber shells from parsing errors.
+  eval 'incr_tap_count_ () { tap_count_=$(($tap_count_ + 1)); }'
+else
+  incr_tap_count_ () { tap_count_=`expr $tap_count_ + 1`; }
+fi
+
+# plan_ NUMBER-OF-PLANNED-TESTS
+# -----------------------------
+# Print a TAP plan for the given number of tests.  This must be called
+# before reporting any test result; in fact, it must be called before
+# emitting anything on standard output.
+plan_ ()
+{
+  echo "1..$1"
+  have_tap_plan_=yes
+}
+
+# late_plan_
+# ----------
+# Print a TAP plan that accounts for the number of tests seen so far.
+# This must be called after all the tests result have been reported;
+# in fact, after this has been called, nothing more can be print on
+# standard output.
+late_plan_ ()
+{
+  echo "1..$tap_count_"
+  have_tap_plan_=yes
+}
+
+# Initialize it to avoid interferences from the environment.
+have_tap_plan_=no
+
+# diag_ [EXPLANATION]
+# ------------------
+# Report the given text as TAP diagnostic.
+diag_ ()
+{
+  test $# -eq 0 || echo "# $*"
+}
+
+# warn_ [EXPLANATION]
+# ------------------
+# Give a warning (using TAP diagnostic).
+warn_ ()
+{
+  diag_ "WARNING:" ${1-"(unknown warning)"} ${1+"$@"}
+}
+
+# result_ RESULT [DESCRIPTION...]
+# -------------------------------
+# Report a test case with the given result.
+result_ ()
+{
+  incr_tap_count_
+  tap_result_=$1; shift
+  echo "$tap_result_" $tap_count_ ${1+"- $*"}
+}
+
+# tap_with_directive_ RESULT DIRECTIVE [-r REASON] [DESCRIPTION...]
+# -----------------------------------------------------------------
+# Write a tap result with the given directive (can be "TODO" or "SKIP").
+# The REASON, if given is appended after the directive.  This function is
+# for internal use only.
+result_with_directive_ ()
+{
+  incr_tap_count_
+  tap_result_=$1; shift
+  tap_directive_=$1; shift
+  case $1 in -r) tap_reason_=" $2" shift 2;; *) tap_reason_="";; esac
+  echo "$tap_result_" $tap_count_ ${1+"- $*"} \
+       "# ${tap_directive_}${tap_reason_}"
+}
+
+# ok_ [DESCRIPTION...]
+# --------------------
+# Report a successful test.
+ok_ ()
+{
+  result_ 'ok' ${1+"$@"}
+}
+
+# not_ok_ [DESCRIPTION...]
+# ------------------------
+# Report a failed test.
+not_ok_ ()
+{
+  result_ 'not ok' ${1+"$@"}
+}
+
+# skip_ [-r REASON] [DESCRIPTION...]
+# ----------------------------------
+# Report a skipped test.  If the `-r' option is present, its argument is
+# give as the reason of the skip.
+skip_ ()
+{
+  result_with_directive_ 'ok' SKIP ${1+"$@"}
+}
+
+# skip_row_ COUNT [-r REASON] [DESCRIPTION...]
+# --------------------------------------------
+# Report a COUNT of skipped test, with the given reason and descriptions
+# (if any).  Useful to avoid cascade failures in case a fair number of
+# tests depend on an earlier one that failed.
+skip_row_ ()
+{
+  skip_count_=$1; shift
+  for i_ in `seq $skip_count_`; do skip_ ${1+"$@"}; done
+}
+
+# xfail_ [-r REASON] [DESCRIPTION...]
+# ----------------------------------
+# Report a test that failed expectedly.  If the `-r' option is present, its
+# argument is give as the reason why the failure is expected.
+xfail_ ()
+{
+  result_with_directive_ 'not ok' TODO ${1+"$@"}
+}
+
+# xpass_ [-r REASON] [DESCRIPTION...]
+# -----------------------------------
+# Report a test that failed unexpectedly.  If the `-r' option is present, its
+# argument is give as the reason why the failure is expected.
+xpass_ ()
+{
+  result_with_directive_ 'ok' TODO ${1+"$@"}
+}
+
+# skip_all_ [REASON ...]
+# ----------------------
+# Skip all the tests in a test script.  Must be used before calling `plan_'
+# or reporting any test result.  Can't be used from within a subshell.
+skip_all_ ()
+{
+  echo "1..0 # SKIP" ${1+"$@"}
+  Exit 0
+}
+
+# bailout_ [REASON ...]
+# ---------------------
+# Stop the execution of the current test suite right now, due to an
+# unrecoverable error.  Can be called at any point, but cannot be used
+# from within a subshell.
+bailout_ ()
+{
+  echo 'Bail out!' ${1+"$@"}
+  Exit 99
+}
+
+# fatal_ [REASON ...]
+# -------------------
+# Same as `bailout_'; for compatibility with `plain-functions.sh'.
+fatal_ ()
+{
+  bailout_ ${1+"$@"}
+}
+
+# framework_failure_ [REASON ...]
+# -------------------------------
+# Stop the execution of the current test suite right now, due to an
+# unrecoverable error in the set-up of the test case.  Can be called
+# at any point, but cannot be used from within a subshell.
+framework_failure_ ()
+{
+  bailout_ "set-up failure"${1+": $*"}
+}
+
+# command_ok_ TEST-DESCRIPTION [--] CMD [ARGS...]
+# -----------------------------------------------
+# Report a passed test if the given command returns with success,
+# a failed test otherwise.
+command_ok_ ()
+{
+  tap_desc_=$1; shift
+  test x"$1" != x"--" || shift
+  if "$@"; then
+    ok_ "$tap_desc_"
+  else
+    not_ok_ "$tap_desc_"
+  fi
+}
+
+# command_not_ok_ TEST-DESCRIPTION [--] CMD [ARGS...]
+# ---------------------------------------------------
+# Report a failed test if the given command returns with success,
+# a failed test otherwise.
+command_not_ok_ ()
+{
+  tap_desc_=$1; shift
+  test x"$1" != x"--" || shift
+  if "$@"; then
+    not_ok_ "$tap_desc_"
+  else
+    ok_ "$tap_desc_"
+  fi
+}
+
+
+# reset_test_count_ COUNT
+# -----------------------
+# Reset the count of the TAP test results seen so far to COUNT.
+# This function is for use in corner cases only (e.g., when `ok_' and
+# `not_ok_' must be used inside a subshell).  Be careful when using it!
+reset_test_count_ ()
+{
+  tap_count_=$1
+}
+
+:
