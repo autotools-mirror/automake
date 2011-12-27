4c38a8cee729ac47aa751364978f8315e1dff1fc
Stefano Lattarini <stefano.lattarini@gmail.com>
2011-12-27 13:31:15 +0100

    cosmetics: be more consistent in copyright notices in tests
    * tests/cond39.test: Update the heading copyright notice, to be
    consistent with the formulation used in the other files.
    * tests/cond40.test: Likewise.
    * tests/cond41.test: Likewise.
    * tests/cond42.test: Likewise.
    * tests/cond43.test: Likewise.
    * tests/conflnk4.test: Likewise.
    * tests/extra8.test: Likewise.
    * tests/extra9.test: Likewise.
    * tests/suffix13.test: Likewise.
    * tests/vala.test: Likewise.
    * tests/vala1.test: Likewise.
    * tests/vala2.test: Likewise.
    * tests/vala3.test: Likewise.
    * tests/vala4.test: Likewise.
    * tests/vala5.test: Likewise.
    * lib/Automake/tests/Condition.pl: Likewise.
    * lib/Automake/tests/Condition-t.pl: Likewise.
    * lib/Automake/tests/DisjConditions.pl: Likewise.
    * lib/Automake/tests/DisjConditions-t.pl: Likewise.
    * lib/Automake/tests/Version.pl: Likewise.
    * lib/Automake/tests/Wrap.pl: Likewise.
    * lib/Automake/tests/Cond2.pl: Add copyright notice.
    * lib/Automake/tests/Cond3.pl: Likewise.
    * lib/Automake/tests/DisjCon2.pl: Likewise.
    * lib/Automake/tests/DisjCon3.pl: Likewise.
    * lib/Automake/tests/Version2.pl: Likewise.
    * lib/Automake/tests/Version3.pl: Likewise.

--- lib/Automake/tests/Condition.pl
+++ lib/Automake/tests/Condition.pl
@@ -1,13 +1,12 @@
-# Copyright (C) 2001, 2002, 2003, 2009  Free Software Foundation, Inc.
+# Copyright (C) 2001, 2002, 2003, 2008, 2009, 2011 Free Software
+# Foundation, Inc.
 #
-# This file is part of GNU Automake.
-#
-# GNU Automake is free software; you can redistribute it and/or modify
+# This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation; either version 2, or (at your option)
 # any later version.
 #
-# GNU Automake is distributed in the hope that it will be useful,
+# This program is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
