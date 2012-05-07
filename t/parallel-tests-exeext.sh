a8f56923e0adc291c0b1cdd556fda17645d3b9f1
Stefano Lattarini <stefano.lattarini@gmail.com>
2012-05-07 16:38:12 +0200

    [ng] refactor: get rid of am__EXEEXT (automake conditional and %transform)
    We can substitute that with a static automake conditional %HANDLE-EXEEXT%
    and a make runtime check on the non-emptiness of $(EXEEXT).
    
    * automake.in (preprocess_file): New transform '%HANDLE-EXEEXT%', TRUE
    if 'EXEEXT' has been AC_SUBST'd, FALSE otherwise.
    (handle_tests): Don't define the transform '%HANDLE-EXEEXT%' anymore
    when processing 'check.am': that is now done transparently by the
    'preprocess_file' function.
    (handle_per_suffix_test): Don't process the transform '%am__EXEEXT%'
    anymore when reading 'check2.am'.  It's superseded by '%HANDLE-EXEEXT%'
    and ...
    * lib/am/check2.am [%HANDLE-EXEEXT%]: ... by a make-runtime check that
    $(EXEEXT) is not empty.
    * m4/init.m4 (AM_INIT_AUTOMAKE): Simplify, taking advantage of the fact
    that we don't need the Automake conditional "am__EXEEXT" anymore.
    * t/parallel-tests-exeext.sh: Tiny simplification.
    
    Signed-off-by: Stefano Lattarini <stefano.lattarini@gmail.com>

--- t/parallel-tests-exeext.sh
+++ t/parallel-tests-exeext.sh
@@ -22,11 +22,8 @@ am_parallel_tests=yes
 
 cat >> configure.ac << 'END'
 dnl We need to fool the init.m4 internals a little.
-AC_DEFUN([_AM_FOO],
-  [m4_provide([_AM_COMPILER_EXEEXT])
-  AC_SUBST([CC], [false])
-  AC_SUBST([EXEEXT])])
-_AM_FOO
+AC_SUBST([CC], [false])
+AC_SUBST([EXEEXT])
 AC_OUTPUT
 END
 
