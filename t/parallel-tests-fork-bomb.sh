d89da9c295ac67ce985270da16f45c4abd1a089a
Stefano Lattarini <stefano.lattarini@gmail.com>
2012-05-12 08:42:37 +0200

    tests: fix spurious failure due to i18n issue
    Fixes automake bug#11452.
    
    * t/parallel-tests-fork-bomb.sh: Export LANG, LANGUAGE and LC_ALL to
    'C', to ensure error messages will be in English, and thus avoiding
    possible false negatives in our grepping of them.
    
    Signed-off-by: Stefano Lattarini <stefano.lattarini@gmail.com>

--- t/parallel-tests-fork-bomb.sh
+++ t/parallel-tests-fork-bomb.sh
@@ -21,6 +21,11 @@
 am_parallel_tests=yes
 . ./defs || Exit 1
 
+# We don't want localized error messages from make, since we'll have
+# to grep them.  See automake bug#11452.
+LANG=C LANGUAGE=C LC_ALL=C
+export LANG LANGUAGE LC_ALL
+
 # The tricky part of this test is to avoid that make hangs or even
 # freezes the system in case infinite recursion (which is the bug we
 # are testing against) is encountered.  The following hacky makefile
