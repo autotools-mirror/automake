ed0f01904b332f0c64f26797eaeb82e5847cc963
Stefano Lattarini <stefano.lattarini@gmail.com>
2012-06-15 23:29:28 +0200

    [ng] texi: be sure to only emit generic pattern rules once
    * lib/am/texibuild.am: Only hold generic pattern rules now, the
    stuff specific to single '.texi' files moved out ...
    * lib/am/texi-spec.am: ... to this new file.
    * Makefile.am (dist_am_DATA): Add it.
    * automake.in (output_texinfo_build_rules): Process 'texi-spec.am'
    rather than 'texbuild.am', and adjust transforms accordingly.
    (handle_texinfo): Process 'texibuild.am', once for each valid Texinfo
    extension (.texi, .txi, .texinfo).
    * t/txinfo-rules-once.sh: New test.
    
    Signed-off-by: Stefano Lattarini <stefano.lattarini@gmail.com>

---
+++ t/txinfo-rules-once.sh
@@ -0,0 +1,36 @@
+#! /bin/sh
+# Copyright (C) 2003-2012 Free Software Foundation, Inc.
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2, or (at your option)
+# any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+# Some grepping checks on Texinfo support.
+
+. ./defs || Exit 1
+
+echo AC_OUTPUT >> configure.ac
+
+$ACLOCAL
+
+: > texinfo.tex
+
+echo info_TEXINFOS = main.texi other.texi sub/x.texi > Makefile.am
+mkdir sub
+echo @setfilename main.info > main.texi
+echo @setfilename other.info > other.texi
+echo @setfilename sub/x.info > sub/x.texi
+$AUTOMAKE
+$EGREP '\.(info|texi)' Makefile.in # For debugging.
+test $(grep -c '^%\.info: %\.texi$' Makefile.in) -eq 1
+
+:
