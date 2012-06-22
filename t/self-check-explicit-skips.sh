3a1a8c6d3cb46bfe65b715ddd8a3e0d7e27e1bf7
Stefano Lattarini <stefano.lattarini@gmail.com>
2012-06-22 11:40:39 +0200

    tests: automatic re-execution works for non-POSIX shells too
    Some inferior shells are too greedy in parsing their input.  If a
    non-POSIX Bourne shell (like Solaris 10 /bin/sh) was used to launch
    one of our test scripts, it would fail unconditionally, because it
    unexpectedly saw some (by it) unsupported constructs, notwithstanding
    such constructs being placed *after* the code implementing automatic
    test re-execution with a better shell.  In conclusion, the shell
    bailed out like this:
    
        $ /bin/sh t/ar.sh
        $ t/ar.sh: syntax error at line 257: `is_newest_files=$' unexpected
    
    By moving all the potentially problematic code in a separate file, to
    be sourced only after the code for automatic re-execution with a better
    shell, we ensure that inferior shell cannot see such code by mistake.
    
    * defs: All code after automatic shell re-execution moved out ...
    * t/ax/test-init.sh: ... to this new file.
    * syntax-checks.mk (xdefs): Add it.
    * Makefile.am (dist_check_DATA): Add it.  Also move in 'defs' from
    a less explicit 'check_DATA' declaration.
    (nodist_check_DATA):  Move in 'defs-static' from a less explicit
    'check_DATA' declaration.
    (check_DATA): Remove.
    * t/self-check-sanity.sh: Remove, it was actually too hacky and brittle,
    sanity-checking situations we don0t actually care about.
    * t/list-of-tests.mk: Adjust.
    * t/self-check-explicit-skips.sh: Adjust, and fix a botched heading
    comments while we are at it.
    * t/self-check-reexec.tap: Adjust.
    * t/self-check-cleanup.tap: Likewise.
    
    Signed-off-by: Stefano Lattarini <stefano.lattarini@gmail.com>

--- t/self-check-explicit-skips.sh
+++ t/self-check-explicit-skips.sh
@@ -15,43 +15,41 @@
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 # Sanity check for the automake testsuite.
-# Check creation/removal of temporary test working directory by './defs'.
+# Check that our testsuite framework can be instructed not to consider
+# an early exit due to some command exiting unexpectedly with status 77.
 
-am_create_testdir=empty
+am_create_testdir=no
 . ./defs || Exit 1
-
-# We still need a little hack to make ./defs work outside automake's
-# tree 'tests' subdirectory.  Not a big deal.
-sed "s|^am_top_builddir=.*|am_top_builddir='$(pwd)'|" \
-  "$am_top_builddir"/defs-static > defs-static
-diff "$am_top_builddir"/defs-static defs-static \
-  && fatal_ "failed to edit defs-static"
-cp "$am_top_builddir"/defs .
 
 set +e
 
 unset am_explicit_skips stderr_fileno_
 AM_TESTS_REEXEC=no; export AM_TESTS_REEXEC
 
-# I'm a lazy typist.
-sh=$AM_TEST_RUNNER_SHELL
+# FIXME: this can be simplified when we improve our runtime checks on the
+# testsuite shell to ensure it supports "VAR=val shell_func" correctly.
+run_dummy_test ()
+{
+  env $2 $AM_TEST_RUNNER_SHELL -c "am_create_testdir=no; . ./defs; $1" \
+                                  dummy.sh
+}
 
-$sh -c '. ./defs; (exit 77); exit 77' dummy.test
+run_dummy_test '(exit 77); exit 77'
 test $? -eq 77 || Exit 1
 
-am_explicit_skips=no $sh -c '. ./defs; sh -c "exit 77"' dummy.test
+run_dummy_test 'sh -c "exit 77"' am_explicit_skips=no
 test $? -eq 77 || Exit 1
 
-am_explicit_skips=yes $sh -c '. ./defs; (exit 77); exit 77' dummy.test
+run_dummy_test '(exit 77); exit 77' am_explicit_skips=yes 
 test $? -eq 78 || Exit 1
 
-am_explicit_skips=y $sh -c '. ./defs; sh -c "exit 77"' dummy.test
+run_dummy_test 'sh -c "exit 77"' am_explicit_skips=y
 test $? -eq 78 || Exit 1
 
-am_explicit_skips=yes $sh -c '. ./defs; Exit 77' dummy.test
+run_dummy_test 'Exit 77' am_explicit_skips=yes
 test $? -eq 77 || Exit 1
 
-am_explicit_skips=y $sh -c '. ./defs; skip_ "foo"' dummy.test
+run_dummy_test 'skip_ "foo"' am_explicit_skips=y
 test $? -eq 77 || Exit 1
 
 :
