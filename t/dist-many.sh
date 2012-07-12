4a22c5a0184a7871ea9cd5225b896b6f51254b63
Stefano Lattarini <stefano.lattarini@gmail.com>
2012-07-13 00:49:39 +0200

    [ng] coverage: distributing lots of files
    * t/dist-many.sh: New test, try distributing ~ 30 thousands files.
    Currently fails on several systems (e.g., Linux 2.6.30 on i686,
    Solaris 10 on i86pc).
    * t/dist-many2.sh: New test, check that our distribution rules do not
    hit errors due to an exceeded command line length when there are
    many files to distribute; it does so by faking  a very low command
    line length limit for 'mkdir' (max 50 arguments) and the shell (max
    4000 characters).  Currently failing.
    
    Signed-off-by: Stefano Lattarini <stefano.lattarini@gmail.com>

---
+++ t/dist-many.sh
@@ -0,0 +1,82 @@
+#! /bin/sh
+# Copyright (C) 2012 Free Software Foundation, Inc.
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2, or (at your option)
+# any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+# Check that our dist rules do not hit errors due to a huge number
+# of files to be distributed.  Checks similar in spirit are done by
+# sister test 'dist-many2.sh', which fakes an artificially low
+# command line length limit for 'mkdir' and the shell.
+
+. ./defs || exit 1
+
+echo AC_OUTPUT >> configure.ac
+
+file=an-empty-file-with-a-long-name
+dir1=a-directory-with-a-long-name
+dir2=another-long-named-directory
+
+# Distributed files will be 3 * $count.
+count=10000
+
+i=1
+while test $i -le $count; do
+  files="
+    $file.$i
+    $dir1.$i/foo
+    $dir2.$i/$file.$i
+  "
+  mkdir $dir1.$i $dir2.$i
+  for f in $files; do
+    : > $f
+    echo $f
+  done
+  i=$(($i + 1))
+  # Disable shell traces after the first iteration, to avoid
+  # polluting the test logs.
+  set +x
+done > t
+set -x # Re-enable shell traces.
+echo 'EXTRA_DIST = \'   >> Makefile.am
+sed 's/$/ \\/' t        >> Makefile.am
+echo '$(am__empty)'     >> Makefile.am
+rm -f t
+
+test $(wc -l <Makefile.am) -eq $(( 2 + (3 * $count) )) \
+  || fatal_ "populating 'EXTRA_DIST'"
+
+$ACLOCAL
+$AUTOCONF
+$AUTOMAKE
+./configure
+
+$MAKE distdir
+
+# Only check head, tail, and a random sample.
+
+test -f $distdir/$file.1
+test -f $distdir/$dir1.1/foo
+test -f $distdir/$dir2.1/$file.1
+
+test -f $distdir/$file.$count
+test -f $distdir/$dir1.$count/foo
+test -f $distdir/$dir2.$count/$file.$count
+
+test -f $distdir/$file.163
+test -f $distdir/$dir1.7645/foo
+test -f $distdir/$dir2.4077/$file.4077
+
+$MAKE distcheck
+
+:
