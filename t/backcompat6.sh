b4e64211f670b024fceb4e1dee8a1f288001307e
Stefano Lattarini <stefano.lattarini@gmail.com>
2012-12-29 00:03:44 +0100

    tests: remove a botched comment from a test case
    * t/backcompat6.sh: Here.
    
    Signed-off-by: Stefano Lattarini <stefano.lattarini@gmail.com>

--- t/backcompat6.sh
+++ t/backcompat6.sh
@@ -15,8 +15,7 @@
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 # Backward-compatibility test: try to build and distribute a package
-# using obsoleted forms of AC_INIT, AM_INIT_AUTOMAKE and AC_OUTPUT,
-# and 'configure.in' as autconf input file.
+# using obsoleted forms of AC_INIT, AM_INIT_AUTOMAKE and AC_OUTPUT.
 # This script can also serve as mild stress-testing for Automake.
 # See also the similar test 'backcompat5.test'.
 
