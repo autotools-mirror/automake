3492f226311377857a54032e43e1a982e7d804bc
Stefano Lattarini <stefano.lattarini@gmail.com>
2013-06-19 11:46:42 +0200

    tests: more significant names for some tests
    * t/extra2.sh: Rename...
    * t/extra-sources-no-spurious.sh: ... like this.
    * t/yflags2.sh: Rename...
    * t/yflags-cxx.sh: ... like this.
    * t/lflags2.sh: Rename...
    * t/lflags-cxx.sh: ... like this.
    
    Signed-off-by: Stefano Lattarini <stefano.lattarini@gmail.com>

--- t/list-of-tests.mk
+++ t/list-of-tests.mk
@@ -448,7 +448,7 @@ t/ext.sh \
 t/ext2.sh \
 t/ext3.sh \
 t/extra.sh \
-t/extra2.sh \
+t/extra-sources-no-spurious.sh \
 t/extra-data.sh \
 t/extra-dist-vpath-dir.sh \
 t/extra-dist-dirs-and-subdirs.sh \
@@ -591,7 +591,7 @@ t/lex-line.sh \
 t/lex-nodist.sh \
 t/lex-pr204.sh \
 t/lflags.sh \
-t/lflags2.sh \
+t/lflags-cxx.sh \
 t/libexec.sh \
 t/libobj-basic.sh \
 t/libobj2.sh \
@@ -1295,7 +1295,7 @@ t/yacc-pr204.sh \
 t/yacc-subdir.sh \
 t/yacc-weirdnames.sh \
 t/yflags.sh \
-t/yflags2.sh \
+t/yflags-cxx.sh \
 t/yflags-cmdline-override.sh \
 t/yflags-conditional.sh \
 t/yflags-d-false-positives.sh \
