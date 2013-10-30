608d1a7908893b2896f5efd2a4ed22d7901262ed
Stefano Lattarini <stefano.lattarini@gmail.com>
2013-10-30 21:02:14 +0000

    tests: expose bug#14991 (relates to 'distcheck')
    * t/distcheck-no-prefix-or-srcdir-override.sh: New, expose the bug.
    * t/list-of-tests.mk (handwritten_TESTS, XFAIL_TESTS): Add it.
    
    Signed-off-by: Stefano Lattarini <stefano.lattarini@gmail.com>

--- t/list-of-tests.mk
+++ t/list-of-tests.mk
@@ -35,6 +35,7 @@ t/gcj6.sh \
 t/override-conditional-2.sh \
 t/override-conditional-pr13940.sh \
 t/dist-pr109765.sh \
+t/distcheck-no-prefix-or-srcdir-override.sh \
 t/instdir-cond2.sh \
 t/java-nobase.sh \
 t/objext-pr10128.sh \
@@ -422,6 +423,7 @@ t/distcheck-hook2.sh \
 t/distcheck-writable-srcdir.sh \
 t/distcheck-missing-m4.sh \
 t/distcheck-outdated-m4.sh \
+t/distcheck-no-prefix-or-srcdir-override.sh \
 t/distcheck-override-infodir.sh \
 t/distcheck-pr9579.sh \
 t/distcheck-pr10470.sh \
