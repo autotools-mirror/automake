9d9aa6d158d85c1c95aed63fd5633977f29d668f
Stefano Lattarini <stefano.lattarini@gmail.com>
2013-12-23 19:12:09 +0100

    tests: remove too-brittle test tap-realtime.sh
    * t/tap-realtime.sh: Delete.  It has always been brittle, but now
    it's also causing spurious failures when mawk is used as the awk
    implementation in tap-driver.sh (see bug#14601).
    * t/list-of-tests.mk: Adjust.
    
    Signed-off-by: Stefano Lattarini <stefano.lattarini@gmail.com>

--- t/list-of-tests.mk
+++ t/list-of-tests.mk
@@ -1149,7 +1149,6 @@ t/tap-planskip-whitespace.sh \
 t/tap-planskip-badexit.sh \
 t/tap-planskip-bailout.sh \
 t/tap-planskip-later-errors.sh \
-t/tap-realtime.sh \
 t/tap-test-number-0.sh \
 t/tap-recheck-logs.sh \
 t/tap-result-comment.sh \
