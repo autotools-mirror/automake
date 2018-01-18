74f779b520da95224ec6e7f36fb033f3698661b9
Mathieu Lirzin <mthl@gnu.org>
2018-01-18 11:00:19 +0100

    tests: Check GCS conformance of 'aclocal' command-line interface
    * t/aclocal.sh: Check that 'aclocal' support the --version and --help
    command-line options.

--- t/aclocal.sh
+++ t/aclocal.sh
@@ -14,20 +14,34 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
-# Test on some aclocal options.  Report from Alexandre Oliva.
+# Test aclocal command-line interface.  Report from Alexandre Oliva.
 
 am_create_testdir=empty
 . test-init.sh
 
 echo "AC_INIT([$me], [0]) AM_INIT_AUTOMAKE" > configure.ac
 
+# Check that aclocal conforms to GNU Coding Standards.
+$ACLOCAL --help
+$ACLOCAL --version
+
+## ----------------- ##
+##  Check --output.  ##
+## ----------------- ##
+
+# Check that --output produce the corresponding file.
 $ACLOCAL --output=fred
 test -f fred
 
+# Check that --output requires an argument.
 $ACLOCAL --output 2>stderr && { cat stderr >&2; exit 1; }
 cat stderr >&2
 grep 'option.*--output.*requires an argument' stderr
 grep '[Tt]ry.*--help.*for more information' stderr
+
+## ----------------------------------- ##
+##  Check Wrong command-line options.  ##
+## ----------------------------------- ##
 
 $ACLOCAL --unknown-option 2>stderr && { cat stderr >&2; exit 1; }
 cat stderr >&2
